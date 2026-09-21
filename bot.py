4b793fae17ecadc68e7e99fd67d74d323b315a4f
dgmburkan1234-dev <dgmburkan1234@gmail.com>
2026-09-21 23:37:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
-import time
+import os
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot aktif!", 200
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_flask, daemon=True).start()import time
 import requests
 import pandas as pd
 import yfinance as yf
